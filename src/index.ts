75b597a11483fe396ef1d3d710a1d0bfee12e067
Rensykes <chiccodallacinacolfurgone@yahoo.it>
2021-11-14 04:36:49 +0100

    Refactor + JSDoc

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,19 @@
 import * as dotenv from "dotenv";
 import axios from "axios";
-//import { CandlesResponse, Candle } from "./types/candles/Candles";
+import { SingleCandleResponse, Candle } from "./types/candles/Candles";
 dotenv.config();
-
-console.log(`${process.env.BITFINEX_API_KEY}`);
-
-// Named this way to avoid clash with the existing Candle class
-type CandleType = [number, number, number, number, number, number];
-
-interface CandlesResponse {
-    [key: number]: CandleType
-}
-
-interface CandlesResponse2 {
-  [key: number]: CandleType
-}
 
 const candles = async () => {
   const baseUrl = "https://api-pub.bitfinex.com/v2/";
   const pathParams = "candles/trade:1h:tLTCUSD/hist";
   const queryParams = "";
-  let res = await axios.get<CandlesResponse[]>(
+  let res = await axios.get<SingleCandleResponse[]>(
     `${baseUrl}/${pathParams}?${queryParams}`
   );
   let { data } = res;
 
-  for (const [key, value] of Object.entries(data)) {
-    //console.log(`${key}: ${value}`);
-  }
-  //console.log(data)
-  //let first = data[119];
-  //console.log(first[1])
-
-  console.log(data[1][0])
-
-  // let candles: Candle[] = data.map(
-  //   (candle) =>
-  //     new Candle(
-  //       candle[0],
-  //       candle[1],
-  //       candle[2],
-  //       candle[3],
-  //       candle[4],
-  //       candle[5]
-  //     )
-  // );
-  //console.log(candles[1].CLOSE)
+  let candles = Candle.candlesBuilder(data);
+  console.log(candles[1].CLOSE);
 };
 
 const symbols = async () => {
--- src/types/candles/Candles.ts
+++ src/types/candles/Candles.ts
@@ -1,10 +1,23 @@
-export interface CandlesResponse {
-  0: number;
-  1: number;
-  2: number;
-  3: number;
-  4: number;
-  5: number;
+/**
+ * Maps the response's internal array
+ *
+ * [
+ *
+ * &nbsp;&nbsp; [ 1636858800000, 256.41, 257.47, 257.63, 256.23, 592.60287106 ],
+ *
+ * &nbsp;&nbsp; ..
+ *
+ * ]
+ *
+ * - Key 0 = MTS
+ * - Key 1 = OPEN
+ * - Key 2 = CLOSE
+ * - Key 3 = HIGH
+ * - Key 4 = LOW
+ * - Key 5 = VOLUME
+ */
+export interface SingleCandleResponse {
+  [key: number]: number;
 }
 
 export class Candle {
@@ -30,4 +43,26 @@ export class Candle {
     this.LOW = LOW;
     this.VOLUME = VOLUME;
   }
+
+  /**
+   * Returns an array of mapped candles from an API call
+   * @param {SingleCandleResponse[]} candlesResponse return type of the candles API call
+   * @returns 
+   */
+  static candlesBuilder = (
+    candlesResponse: SingleCandleResponse[]
+  ): Candle[] => {
+    let candles: Candle[] = candlesResponse.map(
+      (candle) =>
+        new Candle(
+          candle[0],
+          candle[1],
+          candle[2],
+          candle[3],
+          candle[4],
+          candle[5]
+        )
+    );
+    return candles;
+  };
 }
